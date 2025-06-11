cfcc03b33e167e78d462103347aa855f5f313af7
TamaraKosovac <tamarakosovac14@gmail.com>
2025-06-11 22:15:29 +0200

    Initial commit

---
+++ InsuranceSystem/backend/src/main/java/org/unibl/etf/sigurnost/insurancesystem/InsuranceSystemApplication.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.sigurnost.insurancesystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InsuranceSystemApplication.class, args);
+    }
+    
+}
